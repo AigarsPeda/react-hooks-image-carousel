0280bec28a224a922190fb372b09b7f46224b4d3
Aigars Peda <aigarspeda@gmail.com>
2021-02-08 14:25:25 +0200

    Update Carousel.tsx

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -9,17 +9,56 @@ interface Props {
 }
 
 const Carousel: React.FC<Props> = (props) => {
-  const { autoPlay, children: propsChildren } = props;
-  const children = React.Children.toArray(propsChildren);
+  const { autoPlay, children } = props;
+  // const children = React.Children.toArray(propsChildren);
+
   const divRef = useRef<HTMLDivElement>(null);
+  const transitionRef = useRef();
+  // const sliderRef = useRef();
+
   // const getWidth = () => window.innerWidth
+
+  const firstSlide = children[0];
+  const secondSlide = children[1];
+  const lastSlide = children[children.length - 1];
+
+  const getWidth = () => {
+    if (divRef.current) {
+      console.log(divRef.current.getBoundingClientRect().width);
+      return divRef.current.offsetWidth;
+    } else {
+      return 500;
+    }
+  };
 
   const [state, setState] = useState({
     activeIndex: 0,
-    translate: 0,
-    transition: 0.45
+    translate: getWidth(),
+    transition: 0.45,
+    _slides: [lastSlide, firstSlide, secondSlide]
   });
-  const { translate, transition, activeIndex } = state;
+  const { translate, transition, activeIndex, _slides } = state;
+
+  useEffect(() => {
+    transitionRef.current = smoothTransition;
+  });
+
+  useEffect(() => {
+    const slider = divRef.current;
+    const smooth = (e: any) => {
+      if (e.target.className.includes("slider-content")) {
+        transitionRef.current();
+      }
+    };
+
+    if (slider === null) return;
+
+    const transitionEnd = slider.addEventListener("transitionend", smooth);
+
+    return () => {
+      slider.removeEventListener("transitionend", transitionEnd);
+    };
+  });
 
   useEffect(() => {
     if (autoPlay) {
@@ -34,44 +73,48 @@ const Carousel: React.FC<Props> = (props) => {
     }
   });
 
-  const getWidth = () => {
-    if (divRef.current) {
-      console.log(divRef.current.getBoundingClientRect().width);
-      return divRef.current.offsetWidth;
-    } else {
-      return 500;
-    }
-  };
-
   const nextSlide = () => {
-    if (activeIndex === children.length - 1) {
-      return setState({
-        ...state,
-        translate: 0,
-        activeIndex: 0
-      });
-    }
-
     setState({
       ...state,
-      activeIndex: activeIndex + 1,
-      translate: (activeIndex + 1) * getWidth()
+      translate: translate + getWidth(),
+      activeIndex: activeIndex === children.length - 1 ? 0 : activeIndex + 1
     });
   };
 
   const prevSlide = () => {
-    if (activeIndex === 0) {
-      return setState({
-        ...state,
-        translate: (children.length - 1) * getWidth(),
-        activeIndex: children.length - 1
-      });
+    setState({
+      ...state,
+      translate: 0,
+      activeIndex: activeIndex === 0 ? children.length - 1 : activeIndex - 1
+    });
+  };
+
+  useEffect(() => {
+    if (transition === 0) setState({ ...state, transition: 0.45 });
+  }, [state.transition]);
+
+  const smoothTransition = () => {
+    let _slides: any = [];
+
+    // We're at the last slide.
+    if (activeIndex === children.length - 1) {
+      _slides = [children[children.length - 2], lastSlide, firstSlide];
+    }
+
+    // We're back at the first slide. Just reset to how it was on initial render
+    else if (activeIndex === 0) {
+      _slides = [lastSlide, firstSlide, secondSlide];
+    }
+    // Create an array of the previous last slide, and the next two slides that follow it.
+    else {
+      _slides = children.slice(activeIndex - 1, activeIndex + 2);
     }
 
     setState({
       ...state,
-      activeIndex: activeIndex - 1,
-      translate: (activeIndex - 1) * getWidth()
+      _slides,
+      transition: 0,
+      translate: getWidth()
     });
   };
 
@@ -81,11 +124,12 @@ const Carousel: React.FC<Props> = (props) => {
         style={{
           transform: `translateX(-${translate}px)`,
           transition: `transform ease-out ${transition}s`,
-          width: `${getWidth() * children.length}px`,
+          width: `${getWidth() * _slides.length}px`,
           display: "flex"
         }}
+        className="slider-content"
       >
-        {children.map((slide, index) => (
+        {_slides.map((slide, index) => (
           <div key={index} className="carousel-item">
             {slide}
           </div>
